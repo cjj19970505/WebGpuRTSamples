bc272c6a0ce531bd1e66e61335d5fae0bb60e677
陈俊嘉 <cjj19970505@live.cn>
2021-03-27 11:27:47 +0800

    Change shader

--- TexturedCube/shader.hlsl
+++ TexturedCube/shader.hlsl
@@ -2,6 +2,7 @@ struct VertexOut
 {
 	float4 PosH  : SV_POSITION;
 	float2 uv : TEXCOORD0;
+	float4 fragPosition : TEXCOORD1;
 };
 
 cbuffer buffer:register(b0, space0)
@@ -17,10 +18,11 @@ VertexOut VSMain(float4 position : WGPULOCATION0, float2 uv:WGPULOCATION1)
 	VertexOut vout;
 	vout.PosH = mul(modelViewProjectionMatrix, position);
 	vout.uv = uv;
+	vout.fragPosition = 0.5 * (position + float4(1.0, 1.0, 1.0, 1.0));
 	return vout;
 }
 
 float4 PSMain(VertexOut pin) : SV_Target
 {
-	return myTexture.Sample(mySampler, pin.uv);
+	return myTexture.Sample(mySampler, pin.uv) * pin.fragPosition;
 }
