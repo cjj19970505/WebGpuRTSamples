f7e6e884a4a3a26f6924631621ab931b03f924f1
陈俊嘉 <cjj19970505@live.cn>
2021-04-09 16:55:01 +0800

    Add wpf example

---
+++ ComputeBoidsWpf/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComputeBoidsWpf
+{
+    
+    public class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            using(new SampleUwpApp.App())
+            {
+                App app = new App();
+                app.InitializeComponent();
+                app.Run();
+            }
+        }
+    }
+}
